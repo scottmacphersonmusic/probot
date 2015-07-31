c7db21612df78bd81c719a2da4de3d2d7d7008e3
scott <scottmacphersonmusic@gmail.com>
2015-07-30 17:20:41 -0700

    Add create and show actions

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -3,7 +3,28 @@ class RobotsController < ApplicationController
     @robots = Robot.all
   end
 
+  def show
+    @robot = Robot.find(params[:id])
+  end
+
   def new
     @robot = Robot.new
+  end
+
+  def create
+    @robot = Robot.new(robot_params)
+
+    if @robot.save
+      flash[:success] = "Robot created!"
+      redirect_to @robot
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def robot_params
+    params.require(:robot).permit(:name, :job, :height, :weight, :color)
   end
 end
--- test/controllers/robots_controller_test.rb
+++ test/controllers/robots_controller_test.rb
@@ -10,4 +10,19 @@ class RobotsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "should get create" do
+    post :create
+    assert_response :success
+  end
+
+  test "should create bookmark" do
+    assert_difference('Robot.count') do
+      post :create, robot: { name: "Buxley", job: "Compliments",
+                             height: "5", weight: "90", color: "Brown" }
+    end
+    assert_redirected_to robot_path(assigns(:robot))
+    assert_equal "Robot created!", flash[:success]
+  end
+
 end
