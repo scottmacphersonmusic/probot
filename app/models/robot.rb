f71166a12f80eeeba8ccbc92fe8f23ba8d061d68
scott <scottmacphersonmusic@gmail.com>
2015-07-31 20:07:58 -0700

    Add Attachments controller and Attachment model

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,3 +1,4 @@
 class Robot < ActiveRecord::Base
+  has_many :attachments
   validates :name, :job, :height, :weight, :color, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730220441) do
+ActiveRecord::Schema.define(version: 20150801024206) do
+
+  create_table "attachments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "robots", force: :cascade do |t|
     t.string   "name"
